0667eaede6043775609fb03334bcada49554c5b5
Rafael Costa <zrafaelcostaz@gmail.com>
2024-12-03 14:44:07 -0300

    Inclusão de comentários, e correções - Revisão Estrutura Sequencial 4

--- src/Sequencial04.java
+++ src/Sequencial04.java
@@ -7,12 +7,17 @@ public class Sequencial04 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+            // ENTRADAS 
             System.out.println("Salário do funcionário!");
-            System.out.print("Horas trabalhadas (mensais): "); int horasTrabalhadas = sc.nextInt();
-            System.out.print("Valor que recebe (por hora): "); double valorPorHora = sc.nextDouble();
+            System.out.print("Horas trabalhadas (mensais): "); 
+            int horasTrabalhadas = sc.nextInt();
+            System.out.print("Valor que recebe (por hora): "); 
+            double valorPorHora = sc.nextDouble();
 
+            // CÁLCULO
             double salario = horasTrabalhadas * valorPorHora;
 
+            // SAÍDA
             System.out.printf("Salário: R$ %.2f%n", salario);
 
         sc.close();
