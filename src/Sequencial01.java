44352de596e51bf746aec67c791b737221a87cc3
Rafael Costa <zrafaelcostaz@gmail.com>
2024-12-03 14:24:25 -0300

    Revisão Estrutura sequencial 1

--- src/Sequencial01.java
+++ src/Sequencial01.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
 import java.util.Locale;
 
-public class Seq03 {
+public class Sequencial01 {
     public static void main(String[] args) {
-
+        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+            System.out.println("Soma de algoritmos!");
             System.out.print("Digite o valor de A: ");
             int a = sc.nextInt();
             System.out.print("Digite o valor de B: ");
             int b = sc.nextInt();
-            System.out.print("Digite o valor de C: ");
-            int c = sc.nextInt();
-            System.out.print("Digite o valor de D: ");
-            int d = sc.nextInt();
 
-            int diferenca = (a * b - c * d);
+            int soma = a + b;
 
-            System.out.println("Diferença: " + diferenca);
+            System.out.println("Resultado: " + soma);
 
         sc.close();
 
